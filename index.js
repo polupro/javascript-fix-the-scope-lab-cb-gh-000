6d990e328f9d4bb6e0aad8e8e57c3b55c822fa0d
Sergiy  Tkachuk <awertg@mail.ua>
2017-09-03 18:52:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ function add2(n) {
   // Feel free to move things around!
 
 }
-
-var funkyFunction = function() {
 var theFunk = funkyFunction
+var funkyFunction = function() {
+
   return function() {
 
     return "FUNKY!"
